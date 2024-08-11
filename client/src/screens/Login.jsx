c8a6926fddfae941eb1f8cd3ef2a7fac24485640
Florian59c <florian.cagnon@live.fr>
2024-08-11 15:30:53 +0200

    add redirection with buttons

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import './css/Login.css';
 
 function Login() {
     return (
         <div>
             <p>Login page</p>
+
+            <Link to="/register" >
+                <button>se se créer un comte</button>
+            </Link>
         </div>
     );
 }
