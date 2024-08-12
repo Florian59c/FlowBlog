46a919141b459e57e6dba948df20098934530edd
Florian59c <florian.cagnon@live.fr>
2024-08-12 11:22:17 +0200

    feat: don't expose password in entity params

--- server/src/Entity/User.php
+++ server/src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -31,6 +32,7 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Ignore()
      */
     private $password;
 
