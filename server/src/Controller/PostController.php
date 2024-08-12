052ebf7ba63196c128d9749f0516d6fc019a9e4b
Florian59c <florian.cagnon@live.fr>
2024-08-12 17:50:59 +0200

    feat: creating of request for get a post with recently date and create a post

--- server/src/Controller/PostController.php
+++ server/src/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,5 +12,37 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    
+    /**
+     * @Route("/getPostsWithRecentDate", name="getPostsWithRecentDate")
+     */
+    public function getPostsWithRecentDate(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAllWithDate();
+        
+        return $this->json($posts);
+    }
+
+    /**
+     * @Route("/createPost", name="createPost")
+     */
+    public function createPost(PostRepository $postRepository, UserRepository $userRepository, Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $currentUserId = $data['currentUserId'];
+        $currentUser = $userRepository->findOneBy(['id' => $currentUserId]);
+        if ($currentUser->getRole() === "ADMIN") {
+            $post = new Post();
+            $post->setTitle($data['title']);
+            $post->setIntro($data['intro']);
+            $post->setContent($data['content']);
+            $post->setLastDate(new \DateTime('now'));
+            $post->setAuthor($currentUser);
+
+            $postRepository->add($post, true);
+
+            return $this->json(true);
+        } else {
+            return $this->json(false);
+        }
+    }
 }
