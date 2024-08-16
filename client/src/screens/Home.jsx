aa6a5b539151a69e42e176e5965ea8f53ed53ef9
Florian59c <florian.cagnon@live.fr>
2024-08-16 14:50:46 +0200

    feat: style of home page

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -2,6 +2,8 @@ import './css/Home.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import photo from '../assets/img/photo.png';
+import Footer from '../components/Footer';
 
 function Home() {
     const [currentUserId, setCurrentUserId] = useState(0);
@@ -36,43 +38,59 @@ function Home() {
 
     return (
         <div>
-            <div>
-                {currentUserId !== 0 ?
-                    <button
-                        onClick={async () => {
-                            localStorage.removeItem('currentUserId');
-                            setCurrentUserId(0)
-                        }}
-                    >
-                        se déconnecter
-                    </button>
-                    :
-                    <Link to="/login" >
-                        <button>se connecter</button>
-                    </Link>
-                }
-            </div>
-            <Link to="/post-list">
-                <button>Accéder au posts</button>
-            </Link>
-            {currentUser?.role === "ADMIN" ?
-                <div>
-                    <Link to="/post-add">
-                        <button>Ajouter un post</button>
-                    </Link>
+            <section class="py-5">
+                <div class="container overflow-hidden bsb-author-1">
+                    <div class="row justify-content-center gy-4 gy-md-0">
+                        <div class="col-12 col-md-3 col-xl-2 d-flex align-items-center justify-content-center">
+                            <img class="img-fluid rounded-circle author-avatar" loading="lazy" src={photo} alt="Elio Evander" />
+                        </div>
+                        <div class="col-12 col-md-8 col-lg-6 col-xl-5 d-flex align-items-center justify-content-center">
+                            <div class="text-center text-md-start author-content">
+                                <h2 class="fs-2 mb-3">Florian Cagnon</h2>
+                                <p class="mb-3">Diplômé dans le domaine du développement web et passionné par les nouvelles technologies, j'ai toujours été animé par la création d'applications innovantes et l'atteinte d'objectifs ambitieux.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                :
-                ""
-            }
-            {currentUser?.role === "ADMIN" ?
-                <div>
-                    <Link to="/admin-page">
-                        <p>Gestion Administrateur</p>
-                    </Link>
+                <div class="container overflow-hidden bsb-author-1">
+                    <div class="d-grid my-3" className='button-container'>
+                        {currentUserId !== 0 ?
+                            <div className='button'>
+                                <button class="btn btn-primary btn-lg w-100" type="submit"
+                                    onClick={async () => {
+                                        localStorage.removeItem('currentUserId');
+                                        setCurrentUserId(0)
+                                    }}
+                                >
+                                    se déconnecter
+                                </button>
+                            </div>
+                            :
+                            <div className='button'>
+                                <Link to="/login">
+                                    <button class="btn btn-primary btn-lg w-100" type="submit">se connecter</button>
+                                </Link>
+                            </div>
+                        }
+                        <div className='button'>
+                            <Link to="/post-list" className='button'>
+                                <button class="btn btn-primary btn-lg w-100" type="submit">Accéder au posts</button>
+                            </Link>
+                        </div>
+                        {currentUser?.role === "ADMIN" &&
+                            <div className='button'>
+                                <Link to="/post-add" className='button'>
+                                    <button class="btn btn-primary btn-lg w-100" type="submit">Ajouter un post</button>
+                                </Link>
+                            </div>
+                        }
+                    </div>
                 </div>
-                :
-                ""
-            }
+                <div>
+                    {/* le formulaire d'envoie de mail ici */}
+                </div>
+            </section>
+            <Footer currentUserRole={currentUser?.role} />
         </div>
     );
 }
