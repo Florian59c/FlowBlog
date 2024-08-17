b01f21aef8d1cb4730a5db53ae7594ec901cc0de
Florian59c <florian.cagnon@live.fr>
2024-08-17 17:32:18 +0200

    feat: style for post update page

--- client/src/screens/PostUpdate.jsx
+++ client/src/screens/PostUpdate.jsx
@@ -6,7 +6,6 @@ import axios from 'axios';
 function PostUpdate() {
     const navigate = useNavigate();
     const [currentUserId, setCurrentUserId] = useState(0);
-    const [post, setPost] = useState({});
     const [title, setTitle] = useState("");
     const [intro, setIntro] = useState("");
     const [content, setContent] = useState("");
@@ -19,7 +18,6 @@ function PostUpdate() {
         const post = await axios.post('http://localhost:8000/findPost', {
             id: postId
         });
-        setPost(post.data);
         setTitle(post.data.title);
         setIntro(post.data.intro);
         setContent(post.data.content);
@@ -54,103 +52,134 @@ function PostUpdate() {
             }
         } catch (err) {
             console.error(err);
-            setError("un problème est survenu");
+            setErrorDelete("un problème est survenu lors de la suppression");
         }
     };
-
 
     return (
         <div>
             {postId ?
-                <div>
-                    userid : {currentUserId}
-                    postid : {postId}
-                    post : {post.title}
-                    <form
-                        onSubmit={async (e) => {
-                            e.preventDefault();
-                            setConfirm("");
-                            setError("");
-                            try {
-                                if (title !== "" || intro !== "" || content !== "") {
-                                    const updatedPost = await axios.post('http://localhost:8000/updatePost', {
-                                        currentUserId,
-                                        postId,
-                                        title,
-                                        intro,
-                                        content
-                                    });
-                                    if (updatedPost) {
-                                        setConfirm("Le post à bien été modifié");
-                                    } else {
-                                        setError("Vous n'avez pas la permission pour modifier ce post");
-                                    }
-                                } else {
-                                    setError("Vous devez remplir tout les formulaires");
-                                }
-                            } catch (err) {
-                                console.error(err);
-                                setError("un problème est survenu");
-                            }
-                        }}
-                    >
-                        <div>
-                            <label htmlFor="title">
-                                <p>titre : </p>
-                                <input
-                                    type="text"
-                                    id="title"
-                                    name="title"
-                                    value={title}
-                                    onChange={(e) => setTitle(e.target.value)}
-                                />
-                            </label>
-                            <label htmlFor="intro">
-                                <p>Chapô : </p>
-                                <input
-                                    type="text"
-                                    id="intro"
-                                    name="intro"
-                                    value={intro}
-                                    onChange={(e) => setIntro(e.target.value)}
-                                />
-                            </label>
-                            <label htmlFor="content">
-                                <p>contenu : </p>
-                                <input
-                                    type="text"
-                                    id="content"
-                                    name="content"
-                                    value={content}
-                                    onChange={(e) => setContent(e.target.value)}
-                                />
-                            </label>
-                            <div>
-                                <button>Modifier le post</button>
+                <section class="bg-light py-3 py-md-5">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-sm-20 col-md-16 col-lg-12 col-xl-12 col-xxl-10">
+                                <div class="card border border-light-subtle rounded-3 shadow-sm">
+                                    <div class="card-body p-3 p-md-4 p-xl-5">
+                                        <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Formulaire de modification d'un post</h2>
+                                        <form
+                                            onSubmit={async (e) => {
+                                                e.preventDefault();
+                                                setConfirm("");
+                                                setError("");
+                                                try {
+                                                    if (title !== "" || intro !== "" || content !== "") {
+                                                        const updatedPost = await axios.post('http://localhost:8000/updatePost', {
+                                                            currentUserId,
+                                                            postId,
+                                                            title,
+                                                            intro,
+                                                            content
+                                                        });
+                                                        if (updatedPost) {
+                                                            setConfirm("Le post à bien été modifié");
+                                                        } else {
+                                                            setError("Vous n'avez pas la permission pour modifier ce post");
+                                                        }
+                                                    } else {
+                                                        setError("Vous devez remplir tout les formulaires");
+                                                    }
+                                                } catch (err) {
+                                                    console.error(err);
+                                                    setError("un problème est survenu");
+                                                }
+                                            }}
+                                        >
+                                            <div class="row gy-2 overflow-hidden">
+                                                <div class="col-12">
+                                                    <div class="form-floating mb-3">
+                                                        <input
+                                                            type="text"
+                                                            class="form-control"
+                                                            name="title"
+                                                            id="title"
+                                                            placeholder="Titre"
+                                                            value={title}
+                                                            onChange={(e) => setTitle(e.target.value)}
+                                                        />
+                                                        <label for="title" class="form-label">Titre</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12">
+                                                    <div class="form-floating mb-3">
+                                                        <input
+                                                            type="text"
+                                                            class="form-control"
+                                                            name="intro"
+                                                            id="intro"
+                                                            placeholder="Chapô"
+                                                            value={intro}
+                                                            onChange={(e) => setIntro(e.target.value)}
+                                                        />
+                                                        <label for="intro" class="form-label">Chapô</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12">
+                                                    <div class="form-floating mb-3">
+                                                        <textarea
+                                                            type="text"
+                                                            class="form-control"
+                                                            name="content"
+                                                            id="content"
+                                                            placeholder="Contenu"
+                                                            value={content}
+                                                            onChange={(e) => setContent(e.target.value)}
+                                                        />
+                                                        <label for="content" class="form-label">Contenu</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12">
+                                                    <div class="d-grid my-3">
+                                                        <button class="btn btn-primary btn-lg" type="submit">Modifier le post</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <div className='confirm-message'>
+                                            <p>{confirm}</p>
+                                        </div>
+                                        <div className='error-message'>
+                                            <p>{error}</p>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="d-grid my-3">
+                                                <button
+                                                    class="btn btn-danger btn-lg"
+                                                    onClick={deletePost}
+                                                >
+                                                    Supprimer le post
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div className='confirm-message'>
+                                            <p>{confirmDelete}</p>
+                                        </div>
+                                        <div className='error-message'>
+                                            <p>{errorDelete}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            {error}
-                            {confirm}
                         </div>
-                    </form>
-                    <div>
-                        <button
-                            onClick={deletePost}
-                        >
-                            Supprimer le post
-                        </button>
-                        {confirmDelete}
-                        {errorDelete}
                     </div>
-                </div>
+                </section>
                 :
-                <div>
-                    <p>Impossible de trouver le post que vous voulez modifier</p>
+                <div className='unexist'>
+                    <h1>Impossible de trouver le post que vous voulez modifier</h1>
                     <Link to="/">
-                        <button>Retourner à la page d'accueil</button>
+                        <button class="btn btn-primary btn-lg mt-3">Retourner à la page d'accueil</button>
                     </Link>
                 </div>
             }
-
         </div>
     );
 }
